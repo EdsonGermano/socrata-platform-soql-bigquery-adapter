e6846965d7a8d195ab5fcb304c3d323ba11f8c4d
Anna Sapek <anna.sapek@socrata.com>
2015-07-15 11:38:49 -0700

    Cleanup DataSqlizerQuerier

--- soql-server-bq/src/main/scala/com/socrata/bq/query/DataSqlizerQuerier.scala
+++ soql-server-bq/src/main/scala/com/socrata/bq/query/DataSqlizerQuerier.scala
@@ -1,6 +1,5 @@
 package com.socrata.bq.query
 
-import com.google.api.services.bigquery.model.TableRow
 import com.rojoma.simplearm.util._
 import com.socrata.datacoordinator.truth.loader.sql.AbstractRepBasedDataSqlizer
 import com.socrata.datacoordinator.truth.sql.SqlColumnRep
@@ -13,8 +12,6 @@ import com.socrata.soql.SoQLAnalysis
 import com.socrata.soql.types.{SoQLText, SoQLValue}
 import com.typesafe.scalalogging.slf4j.Logging
 import java.sql.{SQLException, PreparedStatement, Connection, ResultSet}
-
-import com.socrata.bq.soql.bqreps.TextRep
 
 import scala.collection.mutable.ArrayBuffer
 import scala.collection.mutable
@@ -30,21 +27,6 @@ trait DataSqlizerQuerier[CT, CV] extends AbstractRepBasedDataSqlizer[CT, CV] wit
                bqReps: Array[SoQLBigQueryReadRep[CT, CV]]) :
                CloseableIterator[com.socrata.datacoordinator.Row[CV]] with RowCount = {
 
-    // get row count
-    val rowCount: Option[Long] =
-      if (reqRowCount) {
-        using(executeSql(conn, toRowCountSql(analysis, dataTableName))) { rs =>
-          try {
-            rs.next()
-            Some(rs.getLong(1))
-          } finally {
-            rs.getStatement.close()
-          }
-        }
-      } else {
-        None
-      }
-
     // For some weird reason, when you iterate over the querySchema, a new Rep is created from scratch
     // every time, which is very expensive.  Move that out of the inner loop of decodeRow.
     // Also, the orderedMap is extremely inefficient and very complex to debug.
@@ -52,8 +34,6 @@ trait DataSqlizerQuerier[CT, CV] extends AbstractRepBasedDataSqlizer[CT, CV] wit
 //    val decoders2 = querySchema.map { case (cid, rep) =>
 //      (cid, rep.fromResultSet(_, _), rep.physColumns.length)
 //    }.toArray
-
-    val repFactory = new BigQueryRepFactory
 
     val decoders = Array(Tuple2(new ColumnId(1), bqReps(0).toSoQL(_)))
 
@@ -64,7 +44,9 @@ trait DataSqlizerQuerier[CT, CV] extends AbstractRepBasedDataSqlizer[CT, CV] wit
 //      new ResultSetIt(rowCount, rs, decodeBigQueryRow(decoders))
 
       val bqResult = BigQueryQuerier.query("socrata-annasapek", "select field1 from [wilbur.test]")
+
       logger.debug("Received " + bqResult.rowCount + " rows from BigQuery")
+
       new BigQueryResultIt(Option(bqResult.rowCount), bqResult, decodeBigQueryRow(decoders))
     } else {
       logger.debug("Queried a dataset with no user columns")
@@ -92,17 +74,13 @@ trait DataSqlizerQuerier[CT, CV] extends AbstractRepBasedDataSqlizer[CT, CV] wit
 
     override def next(): Row[CV] = {
       if (hasNext) {
-        val n = it.next()
-        logger.debug("Next: " + n)
-        toRow(n)
+        toRow(it.next())
       } else {
         throw new Exception("No more data for the BigQueryResultSetIt")
       }
     }
 
     override def hasNext: Boolean = {
-      logger.debug("Rows iterator: " + it)
-      logger.debug("Has next: " + it.hasNext)
       it.hasNext
     }
 
@@ -114,69 +92,6 @@ trait DataSqlizerQuerier[CT, CV] extends AbstractRepBasedDataSqlizer[CT, CV] wit
     def hasNext = false
     def next() = throw new Exception("Called next() on an empty iterator")
     def close() {}
-  }
-
-  def decodeRow(decoders: Array[(ColumnId, (ResultSet, Int) => CV, Int)])(rs: ResultSet):
-    com.socrata.datacoordinator.Row[CV] = {
-
-    val row = new MutableRow[CV]
-    var i = 1
-
-    decoders.foreach { case (cid, rsExtractor, physColumnsLen) =>
-      row(cid) = rsExtractor(rs, i)
-      i += physColumnsLen
-    }
-    row.freeze()
-  }
-
-  private def executeSql(conn: Connection, pSql: ParametricSql): ResultSet = {
-    try {
-      logger.debug("sql: {}", pSql.sql)
-      // Statement to be closed by caller
-      val stmt = conn.prepareStatement(pSql.sql)
-      // need to explicitly set a fetch size to stream results
-      stmt.setFetchSize(1000)
-      pSql.setParams.zipWithIndex.foreach { case (setParamFn, idx) =>
-        setParamFn(Some(stmt), idx + 1)
-      }
-      stmt.executeQuery()
-    } catch {
-      case ex: SQLException =>
-        logger.error(s"SQL Exception on ${pSql}")
-        throw ex
-    }
-  }
-
-  class ResultSetIt(val rowCount: Option[Long], rs: ResultSet, toRow: (ResultSet) => com.socrata.datacoordinator.Row[CV])
-    extends CloseableIterator[com.socrata.datacoordinator.Row[CV]] with RowCount {
-
-    private var _hasNext: Option[Boolean] = None
-
-    def hasNext: Boolean = {
-      _hasNext = _hasNext match {
-        case Some(b) => _hasNext
-        case None => Some(rs.next())
-      }
-      _hasNext.get
-    }
-
-    def next(): com.socrata.datacoordinator.Row[CV] = {
-      if (hasNext) {
-        val row = toRow(rs)
-        _hasNext = None
-        row
-      } else {
-        throw new Exception("No more data for the iterator.")
-      }
-    }
-
-    def close() {
-      try {
-        rs.getStatement().close()
-      } finally {
-        rs.close()
-      }
-    }
   }
 }
 
