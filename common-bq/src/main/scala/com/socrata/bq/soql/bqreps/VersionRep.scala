ffb17e0531a9ffd246722a81fa920e67a95fa0ed
Anna Sapek <anna.sapek@socrata.com>
2015-07-28 11:01:12 -0700

    Add reps for SoQLVersion, SoQLID, and SoQLFloatingTimestamp

---
+++ common-bq/src/main/scala/com/socrata/bq/soql/bqreps/VersionRep.scala
@@ -0,0 +1,22 @@
+package com.socrata.bq.soql.bqreps
+
+import com.rojoma.json.v3.ast.{JString, JNull, JValue}
+import com.socrata.bq.soql.{BigQueryWriteRep, BigQueryReadRep}
+import com.socrata.soql.types.{SoQLVersion, SoQLType, SoQLValue}
+
+class VersionRep extends BigQueryReadRep[SoQLType, SoQLValue] with BigQueryWriteRep[SoQLType, SoQLValue] {
+
+  override def repType: SoQLType = SoQLVersion
+
+  override val bigqueryType: String = "INTEGER"
+
+  override def SoQL(value: String): SoQLValue = {
+    // should never be null
+    SoQLVersion(value.toLong)
+  }
+
+  override def jvalue(value: SoQLValue): JValue = {
+    if (value == null) JNull
+    else JString(value.asInstanceOf[SoQLVersion].value.toString)
+  }
+}
