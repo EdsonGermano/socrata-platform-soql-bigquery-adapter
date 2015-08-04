82311c8cab8856e0f3b1cd2d6fd39a2d24d5c0ad
George Matter <george.matter@socrata.com>
2015-08-04 15:55:03 -0700

    add job-checking logic to BBQResyncHandler

--- store-bq/src/main/scala/com.socrata.bq.store/BBQResyncHandler.scala
+++ store-bq/src/main/scala/com.socrata.bq.store/BBQResyncHandler.scala
@@ -13,17 +13,55 @@ import com.typesafe.scalalogging.slf4j.Logging
 
 import com.google.api.client.googleapis.json.GoogleJsonResponseException
 import com.google.api.client.http.ByteArrayContent
-import com.google.api.services.bigquery.Bigquery
+import com.google.api.services.bigquery.{Bigquery, BigqueryRequest}
 import com.google.api.services.bigquery.model._
 
-class BBQResyncHandler(bqProjectId: String, bqDatasetId: String) extends Logging {
+object BBQResyncHandler {
+  private var die = true
+  private def shouldDie: Boolean = {
+    if (die) {
+      die = false
+      true
+    } else {
+      false
+    }
+  }
+}
+
+class BBQResyncHandler(bigquery: Bigquery, bqProjectId: String, bqDatasetId: String) extends Logging {
+
+  class BBQJob(job: Job) {
+    val jobId = job.getJobReference.getJobId
+    var state = job.getStatus.getState
+    logger.debug(s"new bigquery job ${jobId} with state ${state}")
+
+    def checkStatus: String = {
+      val job = bigquery.jobs.get(bqProjectId, jobId).setFields("status").execute()
+      val newState = job.getStatus.getState
+      if (newState != state) {
+        logger.debug(s"new state for job ${jobId}: ${newState}")
+        state = newState
+      }
+      if (!List("PENDING", "RUNNING", "DONE").contains(state)) {
+        logger.info(s"unexpected job state: ${state}")
+      }
+      state
+    }
+  }
 
   private def parseDatasetId(datasetInternalName: String) = {
     datasetInternalName.split('.')(1).toInt
   }
 
-  def handle(bigquery: Bigquery,
-             datasetInfo: DatasetInfo,
+  private def executeAndAcceptResponseCodes[T](request: BigqueryRequest[T], acceptableResponseCodes: Int*) : Option[T] = {
+    try {
+      Some(request.execute())
+    } catch {
+      case e: GoogleJsonResponseException if acceptableResponseCodes contains e.getDetails.getCode => None
+    }
+  }
+
+  def handle(datasetInfo: DatasetInfo,
              copyInfo: SecondaryCopyInfo,
              schema: ColumnIdMap[SecondaryColumnInfo[SoQLType]],
              managedRowIterator: Managed[Iterator[ColumnIdMap[SoQLValue]]]) : Unit = {
@@ -36,24 +74,16 @@ class BBQResyncHandler(bqProjectId: String, bqDatasetId: String) extends Logging
     val table = new Table()
             .setTableReference(ref)
             .setSchema(bqSchema)
-    // try to create the table
-    try {
-      bigquery.tables.insert(bqProjectId, bqDatasetId, table).execute()
-      logger.info(s"Inserting into ${ref.getTableId}")
-    } catch {
-      case e: GoogleJsonResponseException => {
-        if (e.getDetails.getCode == 409) {
-          // Table already exists. Replace it.
-          bigquery.tables.update(bqProjectId, bqDatasetId, ref.getTableId, table).execute()
-        } else {
-          throw e
-        }
-      }
-    }
+    // delete the table, just in case it already exists (from a failed resync, perhaps)
+    val delete = bigquery.tables.delete(bqProjectId, bqDatasetId, ref.getTableId)
+    executeAndAcceptResponseCodes(delete, 404)
+    // create the table
+    val insert = bigquery.tables.insert(bqProjectId, bqDatasetId, table)
+    executeAndAcceptResponseCodes(insert, 400)
+    logger.info(s"Inserting into ${ref.getTableId}")
 
-    var truncate = true
     for { rowIterator <- managedRowIterator } {
-      val requests =
+      val rows =
         for {
           row: ColumnIdMap[SoQLValue] <- rowIterator
         } yield {
@@ -66,13 +96,30 @@ class BBQResyncHandler(bqProjectId: String, bqDatasetId: String) extends Logging
           JsonUtil.renderJson(rowMap)
         }
 
-      for { batch <- requests.grouped(10000) } {
-        val content = new ByteArrayContent("application/octet-stream", batch.mkString("\n").toCharArray.map(_.toByte))
-        val job = BigqueryUtils.makeLoadJob(ref, truncate)
-        truncate = false
-        val insert = bigquery.jobs.insert(bqProjectId, job, content)
-        insert.execute()
+      val jobIterator =
+        for {
+          batch <- rows.grouped(10000)
+        } yield {
+          val buffer = batch.mkString("\n").toCharArray.map(_.toByte)
+          buffer.update(buffer.length-1, 100) // corrupt the buffer
+          val content = new ByteArrayContent("application/octet-stream", buffer)
+          val job = BigqueryUtils.makeLoadJob(ref)
+          val insert = bigquery.jobs.insert(bqProjectId, job, content).setFields("jobReference,status")
+          val jobResponse = insert.execute()
+          new BBQJob(jobResponse)
+        }
+
+      // force jobs to execute
+      var jobs = jobIterator.toList
+
+      while (jobs.length > 0) {
+        // does this thread sleep block other workers?
+        Thread.sleep(1000 /* 1s */)
+        jobs = jobs.filter { job =>
+          job.checkStatus != "DONE"
+        }
       }
+      logger.debug("done with all jobs")
     }
   }
 }
