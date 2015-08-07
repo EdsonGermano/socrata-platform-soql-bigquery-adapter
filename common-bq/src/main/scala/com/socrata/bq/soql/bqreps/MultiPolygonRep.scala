49d23875157d5ddf994f6b33e57a89a628d91394
Anna Sapek <anna.sapek@socrata.com>
2015-08-07 13:40:08 -0700

    Add BoundingBoxRep object to MultiPolygonRep for extent queries

--- common-bq/src/main/scala/com/socrata/bq/soql/bqreps/MultiPolygonRep.scala
+++ common-bq/src/main/scala/com/socrata/bq/soql/bqreps/MultiPolygonRep.scala
@@ -1,7 +1,7 @@
 package com.socrata.bq.soql.bqreps
 
 import com.rojoma.json.v3.ast.{JString, JValue}
-import com.socrata.bq.soql.BigQueryRep
+import com.socrata.bq.soql.{BigQueryReadRep, BigQueryRep}
 import com.socrata.soql.types._
 import com.vividsolutions.jts.geom.{MultiPolygon, Coordinate, GeometryFactory}
 import com.vividsolutions.jts.io.{WKBReader, WKBWriter}
@@ -9,7 +9,6 @@ import javax.xml.bind.DatatypeConverter.{parseBase64Binary, printBase64Binary}
 
 class MultiPolygonRep extends BigQueryRep[SoQLType, SoQLValue] {
 
-  private val geomFactory = new GeometryFactory()
   private val wkbWriter = new WKBWriter()
   private val wkbReader = new WKBReader()
 
@@ -26,24 +25,34 @@ class MultiPolygonRep extends BigQueryRep[SoQLType, SoQLValue] {
     SoQLMultiPolygon(wkbReader.read(parseBase64Binary(row.head)).asInstanceOf[MultiPolygon])
   }
 
-  // TODO: Add this to a bounding box rep, or use as a special case function for when "EXTENT" sql function is called
-  def boundingBox(row: Seq[String]): SoQLValue = {
-    if (row(0) == null || row(1) == null || row(2) == null || row(3) == null) SoQLNull
-    else {
-      val minLat = row(0).toDouble
-      val minLong = row(1).toDouble
-      val maxLat = row(2).toDouble
-      val maxLong = row(3).toDouble
-      SoQLMultiPolygon(geomFactory.createMultiPolygon(
-        Array(geomFactory.createPolygon(geomFactory.createLinearRing(Array(
-          new Coordinate(minLong, minLat),
-          new Coordinate(minLong, maxLat),
-          new Coordinate(maxLong, maxLat),
-          new Coordinate(maxLong, minLat),
-          new Coordinate(minLong, minLat)
-        ))))))
-    }
-  }
-
   override def numColumns: Int = 1
+}
+
+object MultiPolygonRep {
+  class BoundingBoxRep extends BigQueryReadRep[SoQLType, SoQLValue] {
+
+    private val geomFactory = new GeometryFactory()
+
+    override def repType: SoQLType = SoQLMultiPolygon
+
+    override def SoQL(row: Seq[String]): SoQLValue = {
+      if (row(0) == null || row(1) == null || row(2) == null || row(3) == null) SoQLNull
+      else {
+        val minLat = row(0).toDouble
+        val minLong = row(1).toDouble
+        val maxLat = row(2).toDouble
+        val maxLong = row(3).toDouble
+        SoQLMultiPolygon(geomFactory.createMultiPolygon(
+          Array(geomFactory.createPolygon(geomFactory.createLinearRing(Array(
+            new Coordinate(minLong, minLat),
+            new Coordinate(minLong, maxLat),
+            new Coordinate(maxLong, maxLat),
+            new Coordinate(maxLong, minLat),
+            new Coordinate(minLong, minLat)
+          ))))))
+      }
+    }
+
+    override def numColumns: Int = 4
+  }
 }
