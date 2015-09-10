6cfb4a58a00ee4950804fd72c835a8b3f1904046
Anna Sapek <anna.sapek@socrata.com>
2015-09-10 16:25:13 -0700

    Add soql-server-bq configuration to reference.conf

--- bin/start_bq_adapter.sh
+++ bin/start_bq_adapter.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # Starts the soql-bq-adapter service.
 BASEDIR=$(dirname $0)/..
-CONFIG=${SODA_CONFIG:-$BASEDIR/../docs/onramp/services/soda2.conf}
+# CONFIG=${SODA_CONFIG:-$BASEDIR/../docs/onramp/services/soda2.conf}
+CONFIG=$BASEDIR/soql-server-bq/src/main/resources/reference.conf
 JARFILE=$BASEDIR/soql-server-bq/target/scala-2.10/soql-server-bq-assembly-*.jar
 if [ ! -e $JARFILE ]; then
   cd $BASEDIR && sbt assembly
