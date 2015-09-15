df7852ed448c28f895d86ea744cb9bc484b57159
Justin Harjanto <justinharjanto@hotmail.com>
2015-09-15 08:58:43 -0700

    Delete unnecessary files from soql-server-bq

--- soql-server-bq/src/main/scala/com/socrata/bq/query/BQSchema.scala
+++
@@ -1,9 +0,0 @@
-package com.socrata.bq.query
-
-import com.google.api.services.bigquery.model.TableFieldSchema
-
-// Trait to encapsulate the table schema returned from BigQuery
-trait BQSchema {
-  var tableSchema : List[TableFieldSchema] = null
-}
-
