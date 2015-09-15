2ee72d68b1fb68a880225c1db4e0a5937b0b30d1
Justin Harjanto <justinharjanto@hotmail.com>
2015-09-15 10:33:33 -0700

    Remove unnecessary config fields

--- soql-server-bq/src/main/scala/com/socrata/bq/server/config/QueryServerConfig.scala
+++ soql-server-bq/src/main/scala/com/socrata/bq/server/config/QueryServerConfig.scala
@@ -2,14 +2,12 @@ package com.socrata.bq.server.config
 
 import com.typesafe.config.Config
 import com.socrata.http.server.livenesscheck.LivenessCheckConfig
-import com.socrata.bq.config.StoreConfig
 import com.socrata.thirdparty.curator.{CuratorConfig, DiscoveryConfig}
 import com.socrata.thirdparty.metrics.MetricsOptions
 import com.socrata.thirdparty.typesafeconfig.ConfigClass
 
 class QueryServerConfig(val config: Config, val root: String) extends ConfigClass(config, root) {
   val log4j = getRawConfig("log4j")
-  val store = new StoreConfig(config, path("store"))
   val port = getInt("port")
   val curator = new CuratorConfig(config, path("curator"))
   val discovery = new DiscoveryConfig(config, path("service-advertisement"))
